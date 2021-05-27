ebc9f01a3f0319092fa08d95fa785572a2780dfc
V S <66644986+Serendipity11@users.noreply.github.com>
2021-05-26 21:28:16 -0400

    Created dictionary component and functions to handle submit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Dictionary Propject
+      <div className="container">
+        <header className="App-header"></header>
+        Dictionary
+        <Dictionary />
+        <footer className="text-center">Coded by ToriS</footer>
+      </div>
     </div>
   );
 }
