35e5a8d16276f6f09513928cf76ec96761c86207
V S <66644986+Serendipity11@users.noreply.github.com>
2021-05-30 16:49:17 -0400

    Implemented function that handles audio

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,10 +2,24 @@ import React from "react";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
+  function handleClickAudio(event) {
+    event.preventDefault();
+    let audio = document.getElementById("audio");
+    try {
+      audio.play();
+    } catch (err) {
+      event.target.classList.add("noAudio");
+      console.log("Error: Audio can not be played or doesnt exist");
+    }
+  }
+
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="blank">
-        Listen
+      <audio preload="auto" id="audio">
+        <source src={props.phonetic.audio}></source>
+      </audio>
+      <a href="/" rel="norefferer" className="icons" onClick={handleClickAudio}>
+        🔉
       </a>
       <br />
       {props.phonetic.text}
